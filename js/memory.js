998a7f9e5164a5105a311739fb133426549c8b10
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-03-19 16:43:40 -0300

    shuffles cards, checkIfPair methods

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,30 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedcards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards(cardsArr) {
+    let j, aux = 0
+    for(let i = cardsArr - 1; i >= 1; i -= 1) {
+      j = Math.floor(Math.random() * i)
+      aux = cardsArr[i];
+      cardsArr[i] = cardsArr[j];
+      cardsArr[j] = aux;
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    return false;
+
+  }
+  isFinished() {
+
+  }
 }
